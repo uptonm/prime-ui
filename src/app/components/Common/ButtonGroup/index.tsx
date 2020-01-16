19c8c9143afd7c24e8c32071940ac57096efe06d
Shawn Toubeau <shawntoubeau@gmail.com>
2020-01-15 22:33:35 -0500

    Adds ButtonGroup child validation

--- src/app/components/Common/ButtonGroup/index.tsx
+++ src/app/components/Common/ButtonGroup/index.tsx
@@ -7,17 +7,31 @@ export interface ButtonGroupProps {
 }
 
 class ButtonGroup extends React.Component<ButtonGroupProps> {
-  render() {
+  componentDidMount() {
+
+  }
+
+  validateChildren = () => {
     const { children } = this.props;
     const numChildren = React.Children.count(children);
 
+    // At least be 2 buttons supplied
     if (numChildren < 2) {
       throw Error('Please supply at least 2 Button components to the group');
     }
 
+    // All components are buttons
+    React.Children.forEach(children, (child) => {
+      if (child?.type !== Button) {
+        throw Error('Please only supply valid <Button /> components');
+      }
+    });
+  }
+
+  render() {
     return (
       <div className="btn-group">
-        {children}
+        {this.props.children}
       </div>
     )
   }
